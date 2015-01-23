3c61a1945174ac99705c0c0e22952888e9f8455c
Marli Bells <marli@rockandbloom.com>
2015-01-22 20:32:23 -0600

    remove code from the templates that is supposed to be added with an exercise

--- learnerfiles/ladies-learning-code/index.php
+++ learnerfiles/ladies-learning-code/index.php
@@ -2,7 +2,7 @@
 <div class="section">
   <div class="innerWrapper">
     <div class="left">
-    		<?php get_template_part( 'loop', 'index' );	?>
+    	<?php get_template_part( 'loop', 'index' );	?>
     </div> <!--/left-->
     <?php get_sidebar(); ?>
   </div> <!-- /.innerWrapper -->
